913dae8fd3de129a44163f5f59ae9730a3852af9
Vladimir Ilenko <6730613043.psu.ac.th@gmail.com>
2025-02-14 09:42:35 +0700

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get() //localhost:3000
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/showname') //localhost:3000/showname
+  showName(): string {
+    return this.appService.getName();
+  }
+
+  @Get('/showinformation') //localhost:3000/showinformation
+  getinformation(): string {
+    return this.appService.getinformation();
+  }
+  
+  @Get('/showjson') //localhost:3000/showjson
+  getJson() {
+    return this.appService.getJson();
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'We love NestJS!';
+  }
+
+  getName(): string {
+    return 'Vladimir Ilenko';
+  }
+
+  getinformation(): string {
+    return 'I am Vladimir and I am 16 years old';
+  }
+
+  getJson() {
+    return {
+      name: "Vladimir",
+      lastname: "Ilenko",
+      age: 39,
+    };
+  }
+}
